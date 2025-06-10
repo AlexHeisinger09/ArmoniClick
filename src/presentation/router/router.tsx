981622f729bbd478b2fd0e44ffb256947dd5cb8d
Alex Heisinger Vivanco <heisinger.vivanco@gmail.com>
2025-06-10 14:43:22 -0400

    rutas para iniciar con login

--- src/presentation/router/router.tsx
+++ src/presentation/router/router.tsx
@@ -8,40 +8,40 @@ import { ConfirmAccount } from "../pages/auth/ConfirmAccount";
 
 import { HomeLayout } from "../layouts/HomeLayout";
 import { Home } from "../pages/home/Home";
-import { Calendar }  from "../pages/calendar/Calendar"; // Importa tu componente Calendar
+import { Calendar }  from "../pages/calendar/Calendar";
 import { Patient }  from "../pages/patient/Patient";
 import { Configuration }  from "../pages/configuration/Configuration";
 
 const authRoutes = [
   {
-    to: "/auth/login",
+    to: "/",
     component: <Login />,
   },
   {
-    to: "/auth/registrar",
+    to: "/registrar",
     component: <Register />,
   },
   {
-    to: "/auth/olvide-password",
+    to: "/olvide-password",
     component: <ResetPassword />,
   },
   {
-    to: "/auth/olvide-password/:token",
+    to: "/olvide-password/:token",
     component: <NewPassword />,
   },
   {
-    to: "/auth/confirmar/:id",
+    to: "/confirmar/:id",
     component: <ConfirmAccount />,
   },
 ];
 
 const homeRoutes = [
   {
-    to: "/",
+    to: "/dashboard",
     component: <Home />,
   },
   {
-    to: "/calendario", // Nueva ruta para el calendario
+    to: "/calendario",
     component: <Calendar />,
   },
   {
@@ -55,26 +55,25 @@ const homeRoutes = [
 ];
 
 export const router = createBrowserRouter([
+  // Rutas de autenticación con AuthLayout (incluye login en raíz)
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      ...authRoutes.map((route) => ({
+        path: route.to,
+        element: route.component,
+        ...(route.to === "/" && { index: true }) // Hace que Login sea la ruta index
+      })),
+    ],
+  },
+  
+  // Rutas protegidas con HomeLayout
   {
     path: "/",
     element: <HomeLayout />,
     children: [
       ...homeRoutes.map((route) => ({
-        path: route.to,
-        element: route.component,
-      })),
-      {
-        index: true,
-        path: homeRoutes.at(0)?.to,
-        element: homeRoutes.at(0)?.component,
-      },
-    ],
-  },
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    children: [
-      ...authRoutes.map((route) => ({
         path: route.to,
         element: route.component,
       })),
