8f94ebdd0efc8140701f98e16d6448b8b18d17da
Alex Heisinger Vivanco <heisinger.vivanco@gmail.com>
2025-06-19 18:12:43 -0400

    arreglo backend 4

--- netlify/functions/user/user.ts
+++ netlify/functions/user/user.ts
@@ -4,44 +4,37 @@ import { validateJWT } from "../../middlewares";
 import { HEADERS } from "../../config/utils";
 
 
-export const handler = async (event, context) => {
+const handler: Handler = async (event: HandlerEvent) => {
   const { httpMethod, path } = event;
 
-  // CORS headers para todas las respuestas
-  const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'content-type, authorization',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Content-Type': 'application/json'
-  };
-
-  // Manejar OPTIONS request (preflight)
-  if (httpMethod === 'OPTIONS') {
+  if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
-      headers: corsHeaders,
-      body: ''
+      headers: HEADERS.json,
     };
   }
 
-  // Debug: mostrar todos los headers recibidos
-  console.log('📋 Headers completos:', JSON.stringify(event.headers, null, 2));
-
-  const user = await validateJWT(event.headers.authorization);
+  const user = await validateJWT(event.headers.authorization!);
   if (user.statusCode !== 200) return user;
 
-  if (httpMethod === "GET" && path.includes("/profile")) {
-    return {
-      ...user,
-      headers: corsHeaders
-    };
-  }
+
+
+ if (httpMethod === "GET" && event.path === "/.netlify/functions/user") {
+  return user;
+}
+
+  
+  
+
 
   return {
     statusCode: 405,
     body: JSON.stringify({
       message: "Method Not Allowed",
     }),
-    headers: corsHeaders,
+    headers: HEADERS.json,
   };
 };
+
+
+export { handler };
